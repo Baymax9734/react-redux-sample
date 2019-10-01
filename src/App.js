7c5fa15bdc72a7bca2dbced8cdac78851a399edf
Baymax9734 <whitesnow9734@gmail.com>
2019-10-01 04:45:11 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Posts from './components/Posts';
 
 export default function App() {
   return (
     <div>
-      <h1>React Redux Sample</h1>
+      <Posts />
     </div>
   )
 }
