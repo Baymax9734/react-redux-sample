734955b4e899c83cabbb71c5986d85a1cfa4cfb7
Baymax9734 <whitesnow9734@gmail.com>
2019-10-08 03:48:49 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Posts from './components/Posts';
+import PostForm from './components/Postform';
+
+import './App.css';
 
 export default function App() {
   return (
-    <div>
+    <div className="App">
+      <PostForm />
       <Posts />
     </div>
   )
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Posts extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        const data = res.data;
+        this.setState({ posts : data })
+      })
+  }
+
   render() {
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
+
     return (
       <div>
         <h1>Posts</h1>
+        { postItems }
       </div>
     )
   }
